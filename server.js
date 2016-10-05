204fc575176835f4af1cac54c148ab6093c57bb6
Ajayadasym <ajayadas.ym@gmail.com>
2016-10-05 18:38:41 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/ui/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/Expertise.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Expertise.html'));
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
